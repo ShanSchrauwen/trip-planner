c4f2c9932b929f4c31e614607db35c8fc0edc86f
Shannon Schrauwen <63632791+ShanSchrauwen@users.noreply.github.com>
2022-05-09 14:40:15 +0300

    trying to get it to work again

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const app = express();
+
 const cors = require("cors");
 
 require("dotenv").config({ path: "./config.env" });
 
-
 const port = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(cors({origin: 'http://localhost:8080/'}));
+
 app.use(express.json());
+
 app.use(require("./routes/record"));
 
-
 // get driver connection
-const dbo = require("./db/connect.js");
- 
+const dbo = require("./db/connect");
+
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
- 
+
   });
   console.log(`Server is running on port: ${port}`);
 });
